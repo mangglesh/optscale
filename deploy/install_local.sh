24b0869981aa9ae0c9528fd02f8822b8bcff0b4d
shiv <blue.4209211@gmail.com>
2023-02-19 13:46:18 +0530

    minor fixes for code artifact

--- deploy/install_local.sh
+++ deploy/install_local.sh
@@ -2,13 +2,13 @@
 set -e
 
 
-bash deploy/packages/deploy.sh deploy/config_client
-bash deploy/packages/deploy.sh rest-api/rest_api_client
-bash deploy/packages/deploy.sh rest-api/optscale_exceptions
-bash deploy/packages/deploy.sh rest-api/cloud_adapter
-bash deploy/packages/deploy.sh herald/herald_client
-bash deploy/packages/deploy.sh insider/insider_client
-bash deploy/packages/deploy.sh metroculus/metroculus_client
-bash deploy/packages/deploy.sh auth/auth_client
-bash deploy/packages/deploy.sh rest-api/optscale_types
-bash deploy/packages/deploy.sh katara/katara_client
+#bash deploy/packages/deploy.sh deploy/config_client $1
+bash deploy/packages/deploy.sh rest-api/rest_api_client $1
+bash deploy/packages/deploy.sh rest-api/optscale_exceptions $1
+bash deploy/packages/deploy.sh rest-api/cloud_adapter $1
+bash deploy/packages/deploy.sh herald/herald_client $1
+bash deploy/packages/deploy.sh insider/insider_client $1
+bash deploy/packages/deploy.sh metroculus/metroculus_client $1
+bash deploy/packages/deploy.sh auth/auth_client $1
+bash deploy/packages/deploy.sh rest-api/optscale_types $1
+bash deploy/packages/deploy.sh katara/katara_client $1
--- deploy/packages/deploy.sh
+++ deploy/packages/deploy.sh
@@ -12,7 +12,7 @@ then
     for FILE in ./dist/*
     do
         echo "Uploading File > $FILE";
-        twine upload --repository codeartifact $FILE
+        twine upload $FILE
     done
 fi
 
@@ -21,3 +21,7 @@ rm ./setup.py
 rm -rf ./build
 rm -rf ./dist
 rm -rf ./*.egg-info
+
+#export TWINE_USERNAME=aws
+#export TWINE_PASSWORD=`aws codeartifact get-authorization-token --domain nudgebee --domain-owner 280501305789 --query authorizationToken --output text --profile nudgebee`
+#export TWINE_REPOSITORY_URL=`aws codeartifact get-repository-endpoint --domain nudgebee --domain-owner 280501305789 --repository nudgebee-pypi --format pypi --query repositoryEndpoint --output text --region us-east-1 --profile nudgebee`
