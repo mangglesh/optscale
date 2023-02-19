67fe3c9929f63c4e7220dbc0456fbb1695066ee7
shiv <blue.4209211@gmail.com>
2023-02-19 15:40:23 +0530

    bug fixes in setup.py

--- deploy/packages/setup.py
+++ deploy/packages/setup.py
@@ -1,26 +1,40 @@
 import setuptools
 from setuptools import setup
-import version
 from pathlib import Path
 
+from importlib.machinery import SourceFileLoader
+
 def parse_requirements(filename):
-    """ load requirements from a pip requirements file """
-    lineiter = (line.strip() for line in open(filename))
-    return [line for line in lineiter if line and not line.startswith("#") and not "."]
+    try:
+        """ load requirements from a pip requirements file """
+        lineiter = (line.strip() for line in open(filename))
+        return [line for line in lineiter if line and not line.startswith("#") and not "."]
+    except:
+        print("requirements not found")
+        return []
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_requires = parse_requirements("./requirements.txt")
+name = None
+for s in Path(__name__).parent.glob("./*"):
+    ss = str(s)
+    if ss != "setup.py" and ".egg-info" not in ss:
+        name = ss
+        break
+    
+print("installing module", name)
 
-print(Path(__name__).resolve().parent.name)
+version = SourceFileLoader(f"{name}_version", name+"/version.py").load_module()
 
-name = Path(__name__).resolve().parent.name
+print("module version >>", version.__VERSION__)
+
+install_requires = parse_requirements(f"./{name}/requirements.txt")
 
 setup(
    name=name,
    version=version.__VERSION__,
    author='pollux',
    author_email='nudgebee@pollux.in',
-   #packages=['Watermarkd'],
+   #package_dir={name: "../"+name},
+   packages=[name],
    install_requires=[str(ir.req) for ir in install_requires],
    python_requires='>=3.6'
 )
